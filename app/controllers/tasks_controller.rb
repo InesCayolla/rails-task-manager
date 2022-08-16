5db135a0d2c9fc2b6d1c455b359055d55229651f
inescayolla <ines.cayolla@gmail.com>
2022-08-16 12:45:57 +0200

    create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,26 @@
 class TasksController < ApplicationController
-  
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id].to_i)
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @tasks = Task.new(task_params)
+    @task.save # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to task_path(@tasks)
+  end
+
+  private
+
+  def task_params
+    params.require(:tasks).permit(:title, :details, :completed)
+  end
+
 end
