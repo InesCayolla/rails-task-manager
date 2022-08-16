5c69257322d5aa6ab677e771214297e7dc3d7eef
inescayolla <ines.cayolla@gmail.com>
2022-08-16 15:16:20 +0200

    Solved without CSS~

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,14 +13,33 @@ class TasksController < ApplicationController
 
   def create
     @tasks = Task.new(task_params)
-    @task.save # Will raise ActiveModel::ForbiddenAttributesError
+    @tasks.save # Will raise ActiveModel::ForbiddenAttributesError
     redirect_to task_path(@tasks)
   end
+
+  def edit
+    @task = Task.find(params[:id])
+
+  end
+
+
+def update
+  @tasks = Task.find(params[:id])
+  @tasks.update(task_params) # Will raise ActiveModel::ForbiddenAttributesError
+end
+
+
+def destroy
+  @task = Task.find(params[:id])
+  @task.destroy
+  # No need for app/views/restaurants/destroy.html.erb
+  redirect_to tasks_path, status: :see_other
+end
 
   private
 
   def task_params
-    params.require(:tasks).permit(:title, :details, :completed)
+    params.require(:task).permit(:title, :details, :completed)
   end
 
 end
